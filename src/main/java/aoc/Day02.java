b7bdc5d2d552a7e92e0f9c9ad83dbf47fddcff00
Michelle Jiam <mjiam@student.codam.nl>
2022-12-02 18:47:50 +0100

    Cleaned up day 2

--- src/main/java/aoc/Day02.java
+++ src/main/java/aoc/Day02.java
@@ -9,13 +9,32 @@ Keys for opponents:
 A = Rock
 B = Paper
 C = Scissor
-Keys for self:
-X = Rock
-Y = Paper
-Z = Scissor
+
+R + P = W
+R + S = L
+R + R = D
+P + S = W
+P + R = L
+P + P = D
+S + R = W
+S + P = L
+S + S = D
  */
 
 public class Day02 {
+    private static void calculateTotalScore(String input, Map<String, Integer> symbolMap, Map<String, Integer> comboMap) {
+        Stream<String> lines = input.lines();
+        AtomicInteger totalScore = new AtomicInteger();
+
+        lines.forEach(line -> {
+            // get score for 2nd column symbol
+            int secondColumnScore = symbolMap.get(line.substring(line.length() - 1));
+            // get value of 3rd factor (move/outcome) and add to total
+            totalScore.addAndGet(secondColumnScore + comboMap.get(line));
+        });
+        System.out.println("Total score: " + totalScore);
+    }
+
     private static void solvePartTwo(String input) {
         final Map<String, Integer> outcomeScores = Map.of("X", 0, "Y", 3, "Z", 6);
         final Map<String, Integer> moveMap = Map.of(
@@ -23,35 +42,18 @@ public class Day02 {
                 "B X", 1, "B Y", 2, "B Z", 3,
                 "C X", 2, "C Y", 3, "C Z", 1);
 
-        Stream<String> lines = input.lines();
-        AtomicInteger totalScore = new AtomicInteger();
-
-        lines.forEach(line -> {
-            // get score for outcome move
-            int outcomeScore = outcomeScores.get(line.substring(line.length() - 1));
-            // get move score and add to total
-            totalScore.addAndGet(outcomeScore + moveMap.get(line));
-        });
-        System.out.println("Total score: " + totalScore);
+        calculateTotalScore(input, outcomeScores, moveMap);
     }
+
     private static void solvePartOne(String input) {
         final Map<String, Integer> moveScores = Map.of("X", 1, "Y", 2, "Z", 3);
         final Map<String, Integer> outcomeMap = Map.of(
                 "A Y", 6, "A Z", 0, "A X", 3,
                 "B Z", 6, "B X", 0, "B Y", 3,
                 "C X", 6, "C Y", 0, "C Z", 3);
-        Stream<String> lines = input.lines();
-        AtomicInteger totalScore = new AtomicInteger();
 
-        lines.forEach(line -> {
-            // get score for response move
-            int responseScore = moveScores.get(line.substring(line.length() - 1));
-            // get outcome score and add to total
-            totalScore.addAndGet(responseScore + outcomeMap.get(line));
-        });
-        System.out.println("Total score: " + totalScore);
+        calculateTotalScore(input, moveScores, outcomeMap);
     }
-
 
     public static void main(String[] args) {
         InputGetter inputGetter = new InputGetter();
