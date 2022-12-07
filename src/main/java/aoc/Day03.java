46dbb4dba3a2f123030ecae0eaaaf0f381aec910
Michelle Jiam <mjiam@student.codam.nl>
2022-12-07 17:44:34 +0100

    Improved day 3; added InputGetter List<String> method

--- src/main/java/aoc/Day03.java
+++ src/main/java/aoc/Day03.java
@@ -1,8 +1,10 @@
 package aoc;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Day03 {
     public static int calculatePriority(Character duplicate) {
@@ -14,49 +16,43 @@ public class Day03 {
         }
     }
 
-    private static void solvePartTwo(Scanner input) {
+    private static HashSet<Character> makeSetFromString(String string) {
+        HashSet<Character> set = string.chars()
+                                .mapToObj(c -> (char) c)
+                                .collect(Collectors.toCollection(HashSet::new));
+        return set;
+    }
+
+    private static Character findDuplicate(String s1, String s2, String s3) {
+        HashSet<Character> h1 = makeSetFromString(s1);
+        HashSet<Character> h2 = makeSetFromString(s2);
+        HashSet<Character> h3 = s3 != null ? makeSetFromString(s3) : null;
+
+        h1.retainAll(h2); // reduce set to only duplicates
+        if (h3 != null) {
+            h1.retainAll(h3); // further reduce if comparing 3 strings
+        }
+        return h1.stream().findFirst().get();
+    }
+
+    private static void solvePartTwo(List<String> input) {
         int prioritySum = 0;
-        ArrayList< HashSet<Character> > sacks = new ArrayList<>();
-        sacks.add(new HashSet<Character>()); sacks.add(new HashSet<Character>()); sacks.add(new HashSet<Character>());
-        int elf = 0;
-        while (input.hasNextLine()) {
-            String currentLine = input.nextLine();
-            for(int i = 0; i < currentLine.length(); i++) {
-                sacks.get(elf).add(currentLine.charAt(i));
-            }
-            if (elf == 2) {
-                sacks.get(0).retainAll(sacks.get(1));
-                sacks.get(2).retainAll(sacks.get(0));
-                Character dup = sacks.get(2).stream().findFirst().get();
-                prioritySum += calculatePriority(dup);
-                for(HashSet sack : sacks) {
-                    sack.clear();
-                }
-                elf = 0;
-            }
-            else {
-                elf++;
-            }
+
+        for (int i = 0; i < input.size(); i += 3) {
+            Character duplicate = findDuplicate(input.get(i), input.get(i + 1), input.get(i + 2));
+            prioritySum += calculatePriority(duplicate);
         }
         System.out.println("Sum of priorities: " + prioritySum);
     }
 
-    private static void solvePartOne(Scanner input) {
+    private static void solvePartOne(List<String> input) {
         int prioritySum = 0;
 
-        while (input.hasNextLine()) {
-            String currentLine = input.nextLine();
-            HashSet<Character> h1 = new HashSet<Character>(), h2 = new HashSet<Character>();
-            for(int i = 0; i < currentLine.length()/2; i++) {
-                h1.add(currentLine.charAt(i));
-            }
-            for(int i = currentLine.length()/2; i < currentLine.length(); i++) {
-                h2.add(currentLine.charAt(i));
-            }
-            h1.retainAll(h2);
-            Character dup = h1.stream().findFirst().get();
-
-            prioritySum += calculatePriority(dup);
+        for (String line : input) {
+            String s1 = line.substring(0, line.length() / 2);
+            String s2 = line.substring(line.length() / 2);
+            Character duplicate = findDuplicate(s1, s2, null);
+            prioritySum += calculatePriority(duplicate);
         }
         System.out.println("Sum of priorities: " + prioritySum);
     }
@@ -64,7 +60,11 @@ public class Day03 {
     public static void main(String[] args) {
         InputGetter inputGetter = new InputGetter();
 
-        solvePartOne(inputGetter.getPuzzleInputAsScanner("input/day03.in"));
-        solvePartTwo(inputGetter.getPuzzleInputAsScanner("input/day03.in"));
+        List<String> input = inputGetter.getPuzzleInputAsStringList("input/day03.in");
+        if (input == null)
+            System.exit(1);
+
+        solvePartOne(input);
+        solvePartTwo(input);
     }
 }
--- src/main/java/aoc/InputGetter.java
+++ src/main/java/aoc/InputGetter.java
@@ -3,8 +3,10 @@ package aoc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputGetter {
@@ -21,6 +23,15 @@ public class InputGetter {
     public String getPuzzleInputAsString(String filePath) {
         try {
             return Files.readString(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<String> getPuzzleInputAsStringList(String filePath) {
+        try {
+            return Files.readAllLines(Paths.get(filePath), Charset.forName("UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
