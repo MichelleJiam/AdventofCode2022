8a666ca1856b79b0501d479089c821c7e3cdd814
Michelle Jiam <michelle.l.jiam@gmail.com>
2022-12-11 01:10:52 +0100

    Cleaned up day 10

--- src/main/java/aoc/Day10.java
+++ src/main/java/aoc/Day10.java
@@ -3,39 +3,26 @@ package aoc;
 import java.util.List;
 
 public class Day10 {
-    static int signalStrengthSum = 0;
-    static int cycle = 0;
-    static int x = 1;
-    private static void runCycle() {
+    private static int signalStrengthSum = 0;
+    private static int cycle = 0;
+    private static void runCycle(int x) {
         cycle++;
         if (cycle == 20 || (cycle - 20) % 40 == 0) {
-            System.out.println("On cycle " + cycle + " x is " + x);
             signalStrengthSum += cycle * x;
         }
     }
-//    private static int checkCycle(int cycle, int x) {
-//        if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140
-//                || cycle == 180 || cycle == 220) {
-////            System.out.println("On cycle " + cycle + " x is " + x);
-//            return (cycle * x);
-//        }
-//        return 0;
-//    }
     private static void solvePartOne(List<String> input) {
+        int x = 1;
+
         for (String line : input) {
-//            signalStrengthSum += checkCycle(cycle, x);
             if (line.contains("addx")) {
-//                cycle++;
-//                signalStrengthSum += checkCycle(cycle, x);
-                runCycle();
-                runCycle();
-//                System.out.println(Integer.parseInt(line.split("addx ")[1]));
+                runCycle(x);
+                runCycle(x);
                 x += Integer.parseInt(line.split("addx ")[1]);
             }
-            else {
-                runCycle();
+            else { // if noop
+                runCycle(x);
             }
-//            cycle++;
         }
         System.out.println("Sum of signal strength: " + signalStrengthSum);
     }
